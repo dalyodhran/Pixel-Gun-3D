a944949e3e9a0268e8bfaacabcfa505e8a47c9c7
Odhran Daly <odhran.daly25@mail.dcu.ie>
2020-07-09 21:28:28 +0100

    Connecting to Photon With Player Name

---
+++ Assets/Scripts/LaunchManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class LaunchManager : MonoBehaviourPunCallbacks
+{
+    public void ConnectToPhotonServer()
+    {
+        if (!PhotonNetwork.IsConnected)
+        {
+            PhotonNetwork.ConnectUsingSettings();
+        }
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log(PhotonNetwork.NickName + " Connected to photon server.");
+    }
+
+    public override void OnConnected()
+    {
+        Debug.Log("Conneceted to the internet");
+    }
+}
