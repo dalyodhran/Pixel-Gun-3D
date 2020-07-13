2ba5d9e7537efcc43b76694169501b02015bebb4
Odhran Daly <odhran.daly25@mail.dcu.ie>
2020-07-13 21:11:42 +0100

    Basic FPS Movement [Part-2]

--- Assets/Scripts/MovementController.cs
+++ Assets/Scripts/MovementController.cs
@@ -7,7 +7,19 @@ public class MovementController : MonoBehaviour
     [SerializeField]
     float speed = 5f;
 
+    [SerializeField]
+    private float lookSensativity = 3f;
+
+    [SerializeField]
+    GameObject fpsCamera;
+
     private Vector3 velocity = Vector3.zero;
+
+    private Vector3 rotation = Vector3.zero;
+
+    private float cameraUpAndDownRotation = 0f;
+
+    private float currentCameraUpAndDownRotation = 0f;
 
     private Rigidbody rb;
 
@@ -34,6 +46,19 @@ public class MovementController : MonoBehaviour
 
         //Apply movement
         Move(_movementVelocity);
+
+        //Calculate rotation as a 3D vector for turing around
+        float _yRotation = Input.GetAxis("Mouse X");
+        Vector3 _rotationVector = new Vector3(0, _yRotation, 0) * lookSensativity;
+
+        //Apply rotation
+        Rotate(_rotationVector);
+
+        //Calculate loop up and down camera rotation
+        float cameraUpDownRotation = Input.GetAxis("Mouse Y") * lookSensativity;
+
+        //Apply
+        RotateCamera(cameraUpDownRotation);
     }
 
     #endregion
@@ -46,11 +71,30 @@ public class MovementController : MonoBehaviour
         {
             rb.MovePosition(rb.position + velocity * Time.fixedDeltaTime);
         }
+
+        rb.MoveRotation(rb.rotation * Quaternion.Euler(rotation));
+
+        if (fpsCamera != null)
+        {
+            currentCameraUpAndDownRotation -= cameraUpAndDownRotation;
+            currentCameraUpAndDownRotation = Mathf.Clamp(currentCameraUpAndDownRotation, -85, 85);
+            fpsCamera.transform.localEulerAngles = new Vector3(currentCameraUpAndDownRotation, 0, 0);
+        }
     }
 
     private void Move(Vector3 movementVelocity)
     {
         velocity = movementVelocity;
+    }
+
+    private void Rotate(Vector3 rotationVector)
+    {
+        rotation = rotationVector;
+    }
+
+    private void RotateCamera(float cameraUpAndDownRotation)
+    {
+        this.cameraUpAndDownRotation = cameraUpAndDownRotation;
     }
 
     #endregion
