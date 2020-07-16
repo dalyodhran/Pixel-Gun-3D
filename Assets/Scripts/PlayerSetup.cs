7dfdb89976820a0fa5977aae49b2079c8eecaaf7
Odhran Daly <odhran.daly25@mail.dcu.ie>
2020-07-16 20:45:22 +0100

    Introduction To Player Networking: Movement Synchronization

---
+++ Assets/Scripts/PlayerSetup.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class PlayerSetup : MonoBehaviourPunCallbacks
+{
+    [SerializeField]
+    GameObject FPSCamers;
+
+    #region Unity methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (photonView.IsMine)
+        {
+            transform.GetComponent<MovementController>().enabled = true;
+            FPSCamers.GetComponent<Camera>().enabled = true;
+        }
+        else
+        {
+            transform.GetComponent<MovementController>().enabled = false;
+            FPSCamers.GetComponent<Camera>().enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #endregion
+}
