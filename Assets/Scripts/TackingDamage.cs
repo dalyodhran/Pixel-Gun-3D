7d99b038ad6a4b57025957ff6f47eef4c7533164
Odhran Daly <odhran.daly25@mail.dcu.ie>
2020-07-18 20:54:33 +0100

    Killing Mechanism- RPC Calls

---
+++ Assets/Scripts/TackingDamage.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class TackingDamage : MonoBehaviour
+{
+
+    public float health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = 100;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    [PunRPC]
+    public void TakeDamage(float _damage)
+    {
+        health -= _damage;
+        Debug.Log(health);
+    }
+}
